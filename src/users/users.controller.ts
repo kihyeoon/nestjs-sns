8f012bf7490dda7e98169a3564309b70b136a3ae
kihyeoon <kihyeoon@gmail.com>
2026-02-02 05:47:39 +0900

    feat(users): 유저 서비스, 컨트롤러 생성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body('nickname') nickname: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.usersService.createUser(nickname, email, password);
+  }
+
+  @Get()
+  async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
 }
