3fba570346c9ad2a1862b839a42ddb9c8f292d81
kihyeoon <kihyeoon@gmail.com>
2026-02-04 23:38:11 +0900

    docs(auth): Token Refresh 기능 정리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,6 +32,28 @@ export class AuthService {
    *      3) 검증이 완료되면 찾은 사용자 정보 반환
    */
 
+  /**
+   * 토큰 사용 방식
+   *
+   * 1. 사용자가 로그인, 회원가입을 진행하면 accessToken과 refreshToken을 반환한다.
+   *
+   * 2. 로그인 할때는 Basic 토큰과 함께 요청을 보낸다.
+   *   - Basic 토큰은 email과 password를 base64로 인코딩한 값이다.
+   *   - 예) {authorization: 'Basic <base64(email:password)>'}
+   *
+   * 3. 아무나 접근 할 수 없는 정보 (private route)를 접근 할때는 accessToken을 Header에 담아서 요청을 보낸다.
+   *   - 예) {authorization: 'Bearer <accessToken>'}
+   *
+   * 4. 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 요청을 보낸 사용자가 누구인지 알 수 있다.
+   *   - 예) 현재 로그인 중인 사용자의 포스트만 가져오려면 토큰의 sub 값에 입력돼있는 id를 사용하여 포스트를 조회한다.
+   *
+   * 5. 모든 토큰은 만료 기간이 있다. 만료 기간이 지나면 새로 토큰을 발급받아야한다.
+   *   - 그렇지 않으면 jwtService.verify() 에서 인증 실패한다.
+   *   - accessToken을 새로 발급 받을 수 있는 /auth/token/access와 refreshToken을 새로 발급 받을 수 있는 /auth/token/refresh 엔드포인트가 필요하다.
+   *
+   * 6. 새로운 토큰을 발급 받으면 새로운 토큰을 사용해서 다시 요청을 보내야한다.
+   */
+
   constructor(
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
