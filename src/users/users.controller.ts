ca2dd043fc68a763722535367ad4d9b8a5f4e890
kihyeoon <kihyeoon@gmail.com>
2026-02-04 23:22:00 +0900

    refactor(users): createUser endpoint 삭제(미인증 개발용)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,9 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  async createUser(
-    @Body('nickname') nickname: string,
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return this.usersService.createUser({ nickname, email, password });
-  }
 
   @Get()
   async getAllUsers() {
