6b02681f3634c4dff053e83745502c355f8e9d0f
kihyeoon <kihyeoon@gmail.com>
2026-02-08 13:15:24 +0900

    feat(posts): 개선된 유효성 검사 메시지 추가
    - CreatePostDto의 title 및 content 필드에 대한 유효성 검사 메시지를 한국어로 구체화하여 사용자 친화성을 향상시킴.

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,10 +1,16 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'title은 string 타입이어야 합니다.',
+  })
+  @IsNotEmpty({
+    message: 'title은 비어있을 수 없습니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content은 string 타입이어야 합니다.',
+  })
   content: string;
 }
