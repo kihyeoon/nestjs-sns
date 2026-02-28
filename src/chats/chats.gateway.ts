daa5c0ea8ca35b64a08168cf956990c6cc4905ea
kihyeoon <kihyeoon@gmail.com>
2026-03-01 01:03:34 +0900

    feat(chats): 서버에서 이벤트 전송

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,18 +1,21 @@
 import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { ChatsService } from './chats.service';
-import { CreateChatDto } from './dto/create-chat.dto';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { WebSocketServer } from '@nestjs/websockets';
 
 @WebSocketGateway({ namespace: 'chats' })
 export class ChatsGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly chatsService: ChatsService) {}
 
   handleConnection(client: Socket) {
     console.log('Client connected', client.id);
   }
 
-  @SubscribeMessage('createChat')
-  create(@MessageBody() createChatDto: CreateChatDto) {
-    console.log('createChatDto', createChatDto);
+  @SubscribeMessage('send_message')
+  sendMessage(@MessageBody() message: string) {
+    this.server.emit('receive_message', 'hello from server');
   }
 }
